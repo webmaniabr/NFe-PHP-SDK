28eb88e97092dfa5660478604252b71fede1a563
WebmaniaBR <thyago@webmaniabr.com>
2019-04-24 17:06:06 -0300

    Update emissaoNotaFiscal.php

--- exemplos/emissaoNotaFiscal.php
+++ exemplos/emissaoNotaFiscal.php
@@ -102,20 +102,19 @@ $data['produtos'] = array(
 */
 $data['pedido'] = array(
   'pagamento' => 0, // Indicador da forma de pagamento: 0 - Pagamento à vista ou 1 - Pagamento a prazo
-  'forma_pagamento' => [ 15 ], // Meio de pagamento
+  'forma_pagamento' => [ 15 ], // Meio(s) de pagamento (15 - Boleto Bancário)
   'presenca' => 2, // Indicador de presença do comprador no estabelecimento comercial no momento da operação
   'modalidade_frete' => 0, // Modalidade do frete
   'frete' => '12.56', // Total do frete
   'desconto' => '10.00', // Total do desconto
   'total' => '174.60', // Valor total do pedido pago pelo cliente,
   'pagamento' => 0, // Indicador da forma de pagamento
-  'forma_pagamento' => '15', // Meio de pagamento (15 - Boleto Bancário),
   /*
   // Informações do pagamento (opcional)
-  'valor_pagamento' => '', // Valor do pagamento
-  'cnpj_credenciadora' => '', // // CNPJ da Credenciadora de cartão de crédito/débito
-  'bandeira' => "", // Bandeira da operadora do cartão de crédito/débito
-  'autorizacao' => "", // Número da autorização da operadora de cartão de crédito/débito (NSU)
+  'valor_pagamento' => [], // Valor do pagamento
+  'cnpj_credenciadora' => [], // // CNPJ da Credenciadora de cartão de crédito/débito
+  'bandeira' => [], // Bandeira da operadora do cartão de crédito/débito
+  'autorizacao' => [], // Número da autorização da operadora de cartão de crédito/débito (NSU)
   'tipo_integracao' => 2 // Tipo de integração para pagamento
   */
 );
