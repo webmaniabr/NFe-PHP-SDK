1052b3301ae44f7bbd2cc9d673651534275360d9
WebmaniaBR <thyago@webmaniabr.com>
2019-05-06 17:14:36 -0300

    Tratamento de erros no cURL

--- src/WebmaniaBR/NFe.php
+++ src/WebmaniaBR/NFe.php
@@ -105,11 +105,21 @@ class NFe {
 
     function connectWebmaniaBR( $request, $endpoint, $data ){
 
+        // Set limits
         @set_time_limit( 300 );
         ini_set('max_execution_time', 300);
         ini_set('max_input_time', 300);
         ini_set('memory_limit', '256M');
+        if (
+            strpos($endpoint, '/sefaz/') !== false ||
+            strpos($endpoint, '/certificado/') !== false
+        ){
+            $timeout = 5;
+        } else {
+            $timeout = 300;
+        }
 
+        // Header
         $headers = array(
           'Cache-Control: no-cache',
           'Content-Type:application/json',
@@ -119,9 +129,10 @@ class NFe {
           'X-Access-Token-Secret: '.$this->accessTokenSecret
         );
 
+        // Init connection
         $rest = curl_init();
-        curl_setopt($rest, CURLOPT_CONNECTTIMEOUT , 300);
-        curl_setopt($rest, CURLOPT_TIMEOUT, 300);
+        curl_setopt($rest, CURLOPT_CONNECTTIMEOUT , $timeout);
+        curl_setopt($rest, CURLOPT_TIMEOUT, $timeout);
         curl_setopt($rest, CURLOPT_URL, $endpoint.'?time='.time());
         curl_setopt($rest, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($rest, CURLOPT_SSL_VERIFYPEER, false);
@@ -130,10 +141,33 @@ class NFe {
         curl_setopt($rest, CURLOPT_POSTFIELDS, json_encode( $data ));
         curl_setopt($rest, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($rest, CURLOPT_FRESH_CONNECT, true);
+        curl_setopt($rest, CURLOPT_FOLLOWLOCATION, 1);
+
+        // Connect to API
         $response = curl_exec($rest);
+        $http_status = curl_getinfo($rest, CURLINFO_HTTP_CODE);
+        $curl_errno = (int) curl_errno($rest);
+        if ($curl_errno){
+            $curl_strerror = curl_strerror($curl_errno);
+        }
         curl_close($rest);
 
-        return json_decode($response);
+        // Get cURL errors
+        $curl_error = new StdClass;
+        if ($curl_errno){
+          if (!$http_status){
+            $curl_error->error = 'Não foi possível obter conexão na API da WebmaniaBR®, possível relação com bloqueio no Firewall ou versão antiga do PHP. Verifique junto ao seu programador e hospedagem a comunicação na URL: https://webmaniabr.com/api/. (cURL: '.$curl_strerror.' | PHP: '.phpversion().')';
+          } else {
+            $curl_error->error = 'Não foi possível se conectar na API da WebmaniaBR®. Tente novamente ou mais tarde. (cURL: '.$curl_strerror.' | HTTP Code: '.$http_status.')';
+          }
+        }
+
+        // Return
+        if ($curl_error){
+            return $curl_error;
+        } else {
+            return json_decode($response);
+        }
 
     }
 
